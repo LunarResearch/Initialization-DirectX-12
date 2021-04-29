4b68ab0f5da46a3c8ecfa0bcdac4a2926a8d20e3
tesla707 <tesla707@mail.ru>
2021-04-29 11:09:27 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -144,8 +144,7 @@ LRESULT CALLBACK WindowProc(_In_ HWND hWnd, _In_ UINT message, _In_ WPARAM wPara
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PTCHAR lpCmdLine, _In_ int nShowCmd)
 {
-	WNDCLASSEX wc{};
-	wc.cbSize = sizeof(WNDCLASSEX);
+	WNDCLASSEX wc{ sizeof(WNDCLASSEX) };
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
